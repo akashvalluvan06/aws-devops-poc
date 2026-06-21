a5d788c826992667515a11c06c4bff30fdee19e9
Akash V <akashvalluvan06@gmail.com>
2026-06-21 22:23:01 +0530

    Modified outputs.tf

--- infrastructure-automation/terraform-files/outputs.tf
+++ infrastructure-automation/terraform-files/outputs.tf
@@ -1,9 +1,3 @@
-resource "aws_s3_bucket" "sample_bucket" {
-
-  bucket = lower(var.bucket_name)
-
-  tags = {
-    Name        = var.bucket_name
-    ManagedBy   = "Terraform"
-  }
+output "bucket_name" {
+  value = aws_s3_bucket.sample_bucket.bucket
 }
